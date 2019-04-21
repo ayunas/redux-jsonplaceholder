8ea1f57f85533979b1416803cc48fb89161b8068
ayunas <ayunas@gmail.com>
2019-04-21 12:23:42 -0500

    pulled 100 posts from the JSONplaceholder API stored it to the component state and rendered all the articles to the page along with basic styling

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,19 +2,39 @@ import React from 'react';
 import axios from 'axios';
 
 class Posts extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            posts: []
+        }
+
+    }
     componentWillMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-             .then( res => console.log('axios response: ', res.data))
+             .then( res => {
+                 console.log('axios response: ', res.data)
+                 this.setState({
+                     posts : res.data
+                 })
+                }
+             )
              .catch( err => console.log(err))
     }
 
     render() {
+        console.log("Post State" , this.state);
         return (
-            <h1>Posts</h1>
+            <div>
+                {this.state.posts.map( post => 
+                <article>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </article>
+                )}
+            </div>
         )
     }
-
 }
     
 export default Posts;
